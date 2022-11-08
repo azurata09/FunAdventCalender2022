14fe8e864f37c97b407f9e9dc48834b39ee70cf4
azurata <git@azurata.me>
2022-11-08 10:50:52 +0900

    init

---
+++ src/me/azurata/fac2022/FunAdventCalender2022.java
@@ -0,0 +1,13 @@
+package me.azurata.fac2022;
+
+public class FunAdventCalender2022 {
+
+	/**
+	 * FunAdventCalender2022のメイン関数
+	 * @param args プログラム起動時の引数
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
